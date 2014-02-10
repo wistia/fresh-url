f369fa4e8acf5c24e129c5dee990eaedc6b436f5
Jonathan Kim <me@jonathan-kim.com>
2014-02-10 18:26:57 -0500

    Gulp testing.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    jasmine = require('gulp-jasmine');
 
 gulp.task('default', function(){
     // place code for your default task here
@@ -14,5 +15,11 @@ gulp.task('coffee', function() {
     .pipe(gulp.dest('./dist/'))
     .pipe(concat('fresh_url.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/'))
-})
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('test', function() {
+    gulp.src('spec/javascripts/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(jasmine());
+});
